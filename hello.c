765faa82a27e228e0c486beed14773c50a9e59f1
KiriharaYukiho <2000013136@stu.pku.edu.cn>
2023-12-25 02:52:04 +0800

    fix a bug: forget to renew types at recursion in push_store_inst

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 int main()
 {
-    int a[10];
-    const int len = 10;
-    int b[8 + 2], c, d[len];
-    return 0;
+    int a[3] = {};
+    int b[4] = {0, 1};
+    int c[7] = {2, 8, 6, 3, 9, 1, 5};
+    int d[11];
+    int e[2] = {22, 33}, f[6], g[9] = {85, 0, 1, 29};
+    return 3;
 }
